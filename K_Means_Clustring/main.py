6dc00ae1e0b75b1d8e946c12e299933dd3ff7e52
Sayed Hossein Hosseini Dolat Abadi <s.hossein.hosseini1381@gmail.com>
2024-11-02 18:22:30 +0330

    Update crop and save image section

--- K_Means_Clustring/main.py
+++ K_Means_Clustring/main.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 def initialize_centroids(allPixels):
     choices = np.random.choice(range(len(allPixels)), size=K, replace=False)
-    return [matrix for matrix in choices]
+    return [allPixels[matrix] for matrix in choices]
 
 def k_means_clustring(allPixels):
 
@@ -33,7 +33,7 @@ def update_cluster(allPixels, K, Z):
 
             if j not in Z:
                 # distance calculater
-                distance = np.linalg.norm(allPixels[i] - allPixels[Z[j]])
+                distance = np.linalg.norm(allPixels[i] - Z[j])
 
                 # best cluster
                 if(distance < min_distance and j not in Z):
@@ -46,15 +46,15 @@ def update_cluster(allPixels, K, Z):
 
 def update_centroids(allPixels, K, G):
     #Clear centroids Collection
-    Z = []
+    Z = [k for k in range(K)]
 
     for i in range(K):
-        avrage = np.zeros((256, 1))
+        avreage = np.zeros((256, 1))
         for j in G[i]:
-            avrage += allPixels[j]
+            avreage += allPixels[j]
 
         avrage /= len(G[i])
-        Z.append(avrage)
+        Z[i] = average
 
     return Z
 
@@ -75,14 +75,16 @@ def main():
                 # crop image pixels 16 * 16
                 crop_area = (x * 16, y * 16,(x + 1) * 16,(y + 1) * 16)
                 cropped_image = image.crop(crop_area)
-                cropped_image.save("D://University//Term 7//Linear Algebra//Project//k-means-clustring//DataSet//Cropped_Image//usps_cropped_" + str(picture + 1) + "_" + str(x) + "_" + str(y) + ".jpg")
+
                 # 16 * 16 convert 256 * 1
                 pixel = np.array(cropped_image)
                 shape = np.reshape(pixel, (256, 1))
-                allPixels.append(shape)
 
-    representative = k_means_clustring(allPixels)
-    print(representative)
+                if not np.equal(np.linalg.norm(shape), 0):
+                    allPixels.append(shape)
+
+    Z, G = k_means_clustring(allPixels)
+
 
 if __name__ == "__main__":
     main()
